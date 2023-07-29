60801db3766243833c7d799d2c34368565f0abb8
Filip Was <fwas.corp@gmail.com>
2023-07-29 13:04:30 +0200

    migrated base app into typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css"
+
+function App() {
+
+  return (
+    <div className={styles.baa}>
+      <h1>Panini Creator</h1>
+    </div>
+  );
+}
+
+export default App;
