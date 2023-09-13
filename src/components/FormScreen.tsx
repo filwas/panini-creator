864366b0746303680c3389a125f3bcb55bec74a3
fwas <fwas.corp@gmail.com>
2023-09-13 18:49:29 +0200

    added dice svg

--- src/components/FormScreen.tsx
+++ src/components/FormScreen.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import styles from "./FormScreen.module.css";
 import "@fontsource/instrument-serif";
 import "@fontsource/oxygen-mono";
+import Dices from "./Dices";
 
 function FormScreen() {
   return (
     <div className={styles.wholeFormContainer}>
       <div className={styles.header}>
         Panini Creator
-        <button className={styles.randomizeButton}>randomize panini</button>
+        <button className={styles.randomizeButton}>
+          <Dices className={styles.dices} />
+          randomize panini
+        </button>
       </div>
       <div className={styles.formElementsContainer}>
         {/*these divs below will become react elements later*/}
