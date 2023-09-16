46a5784ec4999ac3cff3aae308befaeded990189
fwas <fwas.corp@gmail.com>
2023-09-16 18:12:12 +0200

    toggler complete

--- src/components/modularForm/FormModule.tsx
+++ src/components/modularForm/FormModule.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FormModule.module.css";
 import "@fontsource/instrument-serif";
 import "@fontsource/oxygen-mono";
 import Dropdown from "../selectorComponents/Dropdown";
 import Carousel from "../selectorComponents/Carousel";
+import Toggle from "../selectorComponents/Toggle";
 
 import  {cheeseVariants}  from "../../data/cheese.js";
 import  {breadVariants}  from "../../data/bread.js";
@@ -13,12 +14,19 @@ interface FormModuleProps {
 }
 
 function FormModule(props: FormModuleProps) {
+
+  function toggleStater(currentState: boolean){
+    console.log(currentState);
+  } 
+
   return (
     <div className={styles.moduleTopWrapper}>
       <div className={styles.moduleTitle}>{props.name}</div>
 
       <Dropdown dropdownOptions={cheeseVariants}></Dropdown>
       <Carousel carouselOptions={breadVariants}></Carousel>
+      <Toggle onStateChange={toggleStater} />
+      <div></div>
     </div>
   );
 }
---
+++ src/components/selectorComponents/Toggle.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styles from "./Toggle.module.css";
+import classNames from "classnames";
+
+interface ToggleProps {
+  initialState?: boolean;
+  onStateChange?: (newState: boolean) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = (props) => {
+  const [state, setState] = useState(true);
+
+  const clickHandler = () => {
+    const newState = !state;
+    setState(newState);
+
+    if (props.onStateChange) {
+      props.onStateChange(newState);
+    }
+  };
+
+  const circle = classNames(
+    styles.circle,
+    state ? styles.circleOn : styles.circleOff
+  );
+
+  return (
+    <div className={styles.toggleWrapper} onClick={clickHandler}>
+      <div className={circle} />
+    </div>
+  );
+};
+
+export default Toggle;
