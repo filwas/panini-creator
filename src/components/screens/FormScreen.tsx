81d30caf7a30be8fe3768155e9b3d70a472ec8b8
fwas <fwas.corp@gmail.com>
2023-10-03 17:06:10 +0200

    PR comments applied

--- src/components/screens/FormScreen.tsx
+++ src/components/screens/FormScreen.tsx
@@ -15,17 +15,17 @@ import classNames from "classnames";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { fromZodError } from "zod-validation-error";
+import { formatData } from "../../api/formatters";
+import { postPayload } from "../../api/client";
 
 function FormScreen() {
   const formMethods = useForm<PaniniFormData>();
   const { handleSubmit } = formMethods;
   const navigate = useNavigate();
-  const onSubmit: SubmitHandler<PaniniFormData> = async (data) => {
+  const onSubmit: SubmitHandler<PaniniFormData> = async (data: PaniniFormData) => {
     try {
-      const parsedData = paniniSchema.parse(formatData(data));
-      console.log(parsedData);
+      const parsedData = paniniSchema.parse(formatData(data)) as SandwichPayload;
       const serverResponse = await postPayload(parsedData);
-      console.log(serverResponse);
       setisBeingTurnedOff(true);
       setTimeout(() => {
         navigate("/success", {state: {imageUrl: serverResponse.imageUrl}});
@@ -67,51 +67,5 @@ function FormScreen() {
 
 export default FormScreen;
 
-function formatData(data: PaniniFormData) {
-  const returnObject: SandwichPayload = {
-    sandwichName: data.sandwichName.toString(),
-    cutlery: data.cutlery[0] ? true : false,
-    napkins: data.napkins[0] ? true : false,
-    base: {
-      bread: data.bread.toString(),
-      cheese: data.cheese,
-      meat: data.meat,
-      dressing: data.dressing,
-      vegetables:
-        data.vegetables[0].length > 0 ? data.vegetables[0].split(",") : [],
-    },
-    extras: {
-      egg: data.egg,
-      spreads: data.spreads[0].length > 0 ? data.spreads[0].split(",") : [],
-      serving: data.serving.toString(),
-      topping: data.topping[0].length > 0 ? data.topping[0] : null,
-    },
-  };
-
-  return returnObject;
-}
 
 
-const postPayload = async (payload: any) => {
-  const url = 'https://training.nerdbord.io/api/v1/panini-creator/order';
-  const headers = {
-    'Authorization': 'secret_token',
-    'Content-Type': 'application/json'
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(payload)
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    } else {
-      return await response.json();
-    }
-  } catch (error) {
-    console.error('There was a problem with the fetch operation: ', error);
-  }
-};
