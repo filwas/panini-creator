6f791e49af3e84461efc3a94f8229de7c8d975ce
fwas <fwas.corp@gmail.com>
2023-10-16 19:36:37 +0200

    testing suite works

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,8 @@
 import { describe, it, expect } from 'vitest';
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import  App  from './App';
+
 
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
@@ -7,5 +11,11 @@ describe('something truthy and falsy', () => {
 
   it('false to be false', () => {
     expect(false).toBe(false);
+  });
+});
+
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import styles from "./App.module.css";
 import SplashScreen from "./components/screens/SplashScreen";
 import FormScreen from "./components/screens/FormScreen";
 import SuccessScreen from "./components/screens/SuccessScreen";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 
 function App() {
-
   return (
-    
-    <div className={styles.mainContainer}>
-      <Routes>
-      <Route path="/" element={<SplashScreen />} />
-      <Route path="/form" element={<FormScreen />} />
-      <Route path="/success" element={<SuccessScreen />} />
-      </Routes>
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+        <div className={styles.mainContainer}>
+          <Routes>
+            <Route path="/" element={<SplashScreen />} />
+            <Route path="/form" element={<FormScreen />} />
+            <Route path="/success" element={<SuccessScreen />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
